17132a8da1be694e5b569793f1d647804fb27106
Семён Колыванов <gdasdsadasdsa@gmail.com>
2025-02-02 01:43:17 +0500

    dropdown scroll

--- KolOS.lua
+++ KolOS.lua
@@ -347,6 +347,7 @@ function Dropdown:new(x, y, width, items, bgColor, textColor)
     obj.expanded = false
     obj.bgColor = bgColor or colors.gray
     obj.textColor = textColor or colors.white
+    obj.scrollOffset = 0
     return obj
 end
 
@@ -369,14 +370,15 @@ function Dropdown:draw(win)
     win.write(displayText)
     if self.expanded then
         local winWidth, winHeight = self.gui.win.getSize()
-        local maxVisibleItems = math.min(#self.items, winHeight - self.y)
+        local maxVisibleItems = math.min(#self.items, 5)
         local startY = self.y + 1
-        if self.y + #self.items > winHeight then
-            startY = self.y - #self.items
+        if self.y + maxVisibleItems > winHeight then
+            startY = self.y - maxVisibleItems
         end
-        for i = 1, #self.items do
+        for i = 1, maxVisibleItems do
             win.setCursorPos(self.x, startY + i - 1)
-            win.write(self.items[i] .. string.rep(" ", self.width - #self.items[i]))
+            local itemIndex = i + self.scrollOffset
+            win.write(self.items[itemIndex] .. string.rep(" ", self.width - #self.items[itemIndex]))
         end
     end
     win.setBackgroundColor(colors.black)
@@ -388,12 +390,13 @@ function Dropdown:handleClick(mx, my)
         self.expanded = not self.expanded
     elseif self.expanded and mx >= self.x and mx < self.x + self.width then
         local winWidth, winHeight = self.gui.win.getSize()
+        local maxVisibleItems = math.min(#self.items, 5)
         local startY = self.y + 1
-        if self.y + #self.items > winHeight then
-            startY = self.y - #self.items
+        if self.y + maxVisibleItems > winHeight then
+            startY = self.y - maxVisibleItems
         end
-        if my >= startY and my < startY + #self.items then
-            self.selectedIndex = my - startY + 1
+        if my >= startY and my < startY + maxVisibleItems then
+            self.selectedIndex = my - startY + 1 + self.scrollOffset
             self.expanded = false
         else
             self.expanded = false
@@ -401,6 +404,33 @@ function Dropdown:handleClick(mx, my)
     else
         self.expanded = false
     end
+end
+
+function Dropdown:handleScroll(direction)
+    if self.expanded then
+        if direction == 0 and self.scrollOffset > 0 then
+            self.scrollOffset = self.scrollOffset - 1
+        elseif direction == 1 and self.scrollOffset < #self.items - 5 then
+            self.scrollOffset = self.scrollOffset + 1
+        end
+    end
+end
+
+function Dropdown:handleKey(key)
+    if self.expanded then
+        if key == keys.up and self.scrollOffset > 0 then
+            self.scrollOffset = self.scrollOffset - 1
+        elseif key == keys.down and self.scrollOffset < #self.items - 5 then
+            self.scrollOffset = self.scrollOffset + 1
+        elseif key == keys.enter then
+            self.selectedIndex = self.scrollOffset + 1
+            self.expanded = false
+        end
+    end
+end
+
+function Dropdown:isExpanded()
+    return self.expanded
 end
 
 -- GUI class
@@ -491,6 +521,12 @@ end
 
 function GUI:handleClick(x, y)
     for _, comp in ipairs(self.components) do
+        if getmetatable(comp) == Dropdown and comp:isExpanded() then
+            comp:handleClick(x, y)
+            return
+        end
+    end
+    for _, comp in ipairs(self.components) do
         if getmetatable(comp) == Button then
             comp:handleClick(x, y)
         elseif getmetatable(comp) == Input then
@@ -504,6 +540,9 @@ end
 function GUI:handleKey(key)
     for _, comp in ipairs(self.components) do
         if getmetatable(comp) == Input and comp.active then
+            return
+        elseif getmetatable(comp) == Dropdown and comp.expanded then
+            comp:handleKey(key)
             return
         end
     end
@@ -528,8 +567,10 @@ function GUI:update(event, param1, param2, param3)
         end
     elseif event == "mouse_scroll" then
         for _, comp in ipairs(self.components) do
-            if getmetatable(comp) == Textarea and comp:isMouseOver(param2, param3) then
-                if param1 == -1 then
+            if getmetatable(comp) == Dropdown and comp.expanded then
+                comp:handleScroll(param1)
+            elseif getmetatable(comp) == Textarea and comp:isMouseOver(param2, param3) then
+                if param1 == 0 then
                     comp:scrollUp()
                 elseif param1 == 1 then
                     comp:scrollDown()
